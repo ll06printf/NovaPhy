1a82a46deeddd41e68c684ab710f5a522ecfed62
ll06 <ll06printf@outlook.com>
2026-03-11 19:06:40 +0800

    Use sccache for Windows

--- compat/compat.cmake
+++ compat/compat.cmake
@@ -1,15 +1,20 @@
-# The module detects the environment and applying some patch.
+# The module detects the environment and applying some tools or 
+# patches to improve the build performance and stability.
 
-# TODO
+# enable compiler cache is available
 
-# enable ccache is available
-
+find_program(SCCACHE sccache)
 find_program(CCACHE ccache)
-if (CCACHE)
+if (SCCACHE)
+    message(STATUS "Using sccache: ${SCCACHE}")
+    set(CMAKE_CXX_COMPILER_LAUNCHER ${SCCACHE})
+    set(CMAKE_C_COMPILER_LAUNCHER ${SCCACHE})
+    set(CMAKE_CUDA_COMPILER_LAUNCHER ${SCCACHE})
+elseif (CCACHE)
     message(STATUS "Using ccache: ${CCACHE}")
     set(CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE})
     set(CMAKE_C_COMPILER_LAUNCHER ${CCACHE})
     set(CMAKE_CUDA_COMPILER_LAUNCHER ${CCACHE})
 else()
-    message(STATUS "ccache not found, not using it.")
+    message(STATUS "compiler cache not found, not using it.")
 endif()
