95ae9140a941bbf10f4b8704e3b3bd422f1af5bf
ll06 <ll06printf@outlook.com>
2026-03-11 18:26:34 +0800

    Use CCache
    - Added CCache support
    - Moved build-linux-gcc.py to scripts dir in project root

--- compat/compat.cmake
+++ compat/compat.cmake
@@ -1,3 +1,15 @@
 # The module detects the environment and applying some patch.
 
 # TODO
+
+# enable ccache is available
+
+find_program(CCACHE ccache)
+if (CCACHE)
+    message(STATUS "Using ccache: ${CCACHE}")
+    set(CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE})
+    set(CMAKE_C_COMPILER_LAUNCHER ${CCACHE})
+    set(CMAKE_CUDA_COMPILER_LAUNCHER ${CCACHE})
+else()
+    message(STATUS "ccache not found, not using it.")
+endif()
